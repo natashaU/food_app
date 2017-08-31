8feb65b7efa90dd4bed9320a257d988e21edbf6a
Natasha Umer <natasha.umer2@gmail.com>
2017-08-31 13:45:20 -0400

    server up and running, index page working

--- server.js
+++ server.js
@@ -1,1 +1,32 @@
- 
+const express = require('express');
+const path = require('path');
+const methodOverride = require('method-override');
+const bodyParser = require('body-parser');
+const logger = require('morgan');
+
+//const itemsRouter = require('./routes/items');
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(bodyParser.urlencoded({
+  extended: false,
+}));
+app.use(bodyParser.json());
+
+app.use(methodOverride('_method'));
+
+//app.use('/items', itemsRouter);
+
+app.get('/', (req, res) => {
+  res.send('hello world');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server up and listening on port ${PORT}, in ${app.get('env')} mode.`);
+});
